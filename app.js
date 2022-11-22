e024ad814c1e3a71fff6a40903657ed5c7fa702d
maryamsalamat <maryamsalamat47@gmail.com>
2022-11-22 15:40:37 +0330

    signUp changed

--- app.js
+++ app.js
@@ -1,6 +1,1 @@
- console.log('hello there')
- console.log('hello there')
- console.log('hello there')
- console.log('hello there')
- console.log('hello there')
- console.log('hello there')
+console.log('sign up')
